b20886237a18c223d679a6b9b78b4ff2dcf6cf3e
Aaron Isaac <aaron@aisaac.co>
2023-07-04 21:47:22 -0700

    Switch to postgres

--- src/main/java/co/aisaac/scaper/ScraperApplication.java
+++ src/main/java/co/aisaac/scaper/ScraperApplication.java
@@ -2,20 +2,9 @@ package co.aisaac.scaper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class ScraperApplication {
-
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain( ServerHttpSecurity http) {
-        return http.authorizeExchange()
-                .pathMatchers("/actuator/**").permitAll()
-                .anyExchange().authenticated()
-                .and().build();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ScraperApplication.class, args);
