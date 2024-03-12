113331ce7a827b854aff621a1abff70756f4a272
Aaron Isaac <aaron@aisaac.co>
2024-03-11 17:23:01 -0700

    Moving to reactJS frontend

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-mvn clean package
-rsync -avz ./target/scraper-frontend-0.0.1.jar aaron@job-scraper.homelab.io:~/scraper-frontend-0.0.1.jar
-ssh aaron@job-scraper.homelab.io "sudo systemctl restart job-scraper.service"
