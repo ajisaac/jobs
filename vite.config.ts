926c5acb83e3639e71417b66c7fbaecbd0eab179
Aaron Isaac <aaron@aisaac.co>
2024-05-05 20:19:09 -0700

    Revert "init commit"
    This reverts commit 54d3125b660eb2d73940b6d054f665b949ae2bba.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// todo what is this
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
