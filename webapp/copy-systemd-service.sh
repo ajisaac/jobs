29a5289c26c3f37e21b17def3a7270d30d6c1ae8
Aaron Isaac <aaron@aisaac.co>
2024-06-03 20:52:08 +0700

    Move to different directory on server

--- webapp/copy-systemd-service.sh
+++ webapp/copy-systemd-service.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-rsync -avz -e "ssh -i ~/.ssh/JobScraperKey.pem" ./jobs.service ubuntu@server-host:~/jobs.service
-ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo cp ~/jobs.service /etc/systemd/system/jobs.service"
+rsync -avz -e "ssh -i ~/.ssh/JobScraperKey.pem" ./jobs.service ubuntu@server-host:~/job-scraper/jobs.service
+ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo cp ~/job-scraper/jobs.service /etc/systemd/system/jobs.service"
 ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo systemctl stop jobs.service"
 ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo systemctl disable jobs.service"
 ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo systemctl start jobs.service"
--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
 mvn clean package
-rsync -avz -e "ssh -i ~/.ssh/JobScraperKey.pem" ./target/jobs-0.0.1.jar ubuntu@server-host:~/jobs-0.0.1.jar
-ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo mv ~/jobs-0.0.1.jar /home/aaron/jobs-0.0.1.jar"
-ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo chown aaron:aaron /home/aaron/jobs-0.0.1.jar"
+rsync -avz -e "ssh -i ~/.ssh/JobScraperKey.pem" ./target/webapp-0.0.1.jar ubuntu@server-host:/home/ubuntu/job-scraper/webapp-0.0.1.jar
 ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo systemctl restart jobs.service"
