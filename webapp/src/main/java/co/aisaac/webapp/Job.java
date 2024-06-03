22e4190a4b49033e2a45a77701fef97f1cf68b61
Aaron Isaac <aaron@aisaac.co>
2024-06-03 20:50:52 +0700

    Migrate to postgres

--- webapp/src/main/java/co/aisaac/webapp/Job.java
+++ webapp/src/main/java/co/aisaac/webapp/Job.java
@@ -1,5 +1,8 @@
 package co.aisaac.webapp;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,17 +10,22 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@ToString
 @Getter
 @Setter
 @Entity(name = "jobs")
 public class Job {
 
     @Id
+    @SequenceGenerator(name = "jobs_id_generator", sequenceName = "jobs_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "jobs_id_generator")
+    @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
     public String title;
