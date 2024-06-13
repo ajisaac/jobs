cf9b96e209c82109d7ea44da5b0c70d8301c59ba
Aaron Isaac <aaron@aisaac.co>
2024-06-14 05:04:43 +0700

    Add users to application

---
+++ webapp/src/main/java/co/aisaac/webapp/UserRepository.java
@@ -0,0 +1,10 @@
+package co.aisaac.webapp;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findAllByEmail(String username);
+}
