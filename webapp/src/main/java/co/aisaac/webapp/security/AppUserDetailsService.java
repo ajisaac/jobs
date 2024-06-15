f5b6e5b9b97a46579003849c05cb0c733c418941
Aaron Isaac <aaron@aisaac.co>
2024-06-16 06:29:27 +0700

    Move some files

--- webapp/src/main/java/co/aisaac/webapp/security/AppUserDetailsService.java
+++ webapp/src/main/java/co/aisaac/webapp/security/AppUserDetailsService.java
@@ -1,7 +1,5 @@
 package co.aisaac.webapp.security;
 
-import co.aisaac.webapp.User;
-import co.aisaac.webapp.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
