b780eda74fafcb5add1222c1751eed5a12c9928c
Aaron Isaac <aaron@aisaac.co>
2024-06-16 06:52:40 +0700

    Revert "Move some files"
    This reverts commit f5b6e5b9b97a46579003849c05cb0c733c418941.

--- webapp/src/main/java/co/aisaac/webapp/security/AppUserDetailsService.java
+++ webapp/src/main/java/co/aisaac/webapp/security/AppUserDetailsService.java
@@ -1,5 +1,7 @@
 package co.aisaac.webapp.security;
 
+import co.aisaac.webapp.User;
+import co.aisaac.webapp.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
