7e2125ed014dc059b368bf40e92466cc74dee891
Aaron Isaac <aaron@aisaac.co>
2024-12-29 22:14:13 -0500

    Remove server deployment stuff

--- webapp/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-mvn clean package
-rsync -avz -e "ssh -i ~/.ssh/JobScraperKey.pem" ./target/webapp-0.0.1.jar ubuntu@server-host:/home/ubuntu/job-scraper/webapp-0.0.1.jar
-ssh -i ~/.ssh/JobScraperKey.pem ubuntu@server-host "sudo systemctl restart jobs.service"
