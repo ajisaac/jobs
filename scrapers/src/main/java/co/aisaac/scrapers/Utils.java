03920d046ce48e641e5b08cb92baa65236e1ac1c
Aaron Isaac <aaron@aisaac.co>
2024-12-30 20:11:16 -0500

    Use new method for thread sleeping

--- scrapers/src/main/java/co/aisaac/scrapers/Utils.java
+++ scrapers/src/main/java/co/aisaac/scrapers/Utils.java
@@ -1,6 +1,7 @@
 package co.aisaac.scrapers;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Utils {
 	public static void sleepRandom(int maxSeconds) {
@@ -10,7 +11,7 @@ public class Utils {
 	public static void sleep(long seconds) {
 		try {
 			System.out.println("Sleeping for " + seconds + " seconds");
-			Thread.sleep(seconds * 1000);
+			TimeUnit.SECONDS.sleep(seconds);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
