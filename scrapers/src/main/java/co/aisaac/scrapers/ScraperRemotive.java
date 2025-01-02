614342987927dd22aff8a5ca5f48036b73eb1fc6
Aaron Isaac <aaron@aisaac.co>
2025-01-02 16:09:12 -0500

    Remove remotive and remoteok

--- scrapers/src/main/java/co/aisaac/scrapers/ScraperRemotive.java
+++ scrapers/src/main/java/co/aisaac/scrapers/ScraperRemotive.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class ScraperRemotive {
 
-    private final WebDriver driver;
+    private WebDriver driver;
     private final Database db;
 
     private static final String url = "https://remotive.com/remote-jobs/software-dev";
@@ -44,7 +44,7 @@ public class ScraperRemotive {
     public void scrape() {
         System.out.println("Scraping main page");
 
-        List<WebElement> elements = driver.findElements(By.cssSelector("a[href^='/remote-jobs/software-dev/']"));
+        List<WebElement> elements = driver.findElements(By.cssSelector("a[href*='/remote-jobs/software-dev/']"));
         Map<String, Job> jobs = new HashMap<>();
 
 
@@ -71,35 +71,53 @@ public class ScraperRemotive {
         }
 
 
+        int i = 0, j = 0;
         for (Job job : jobs.values()) {
+            System.out.println("\nScraping attempt: " + i++ + " - Actual: " + j + " - " + job.url);
 
             if (db.hrefExists(job.url)) {
                 System.out.println(job.url + " already exists");
                 continue;
             }
-            System.out.println("Scraping " + job.url);
 
-            try {
-                scrapeJob(job);
-            } catch (Exception e) {
-                System.out.println("Failed to scrape job " + job.url);
-                System.out.println(e.getMessage());
+            boolean success = scrapeJob(job);
+            if (success) {
+                j++;
             }
-
         }
     }
 
-    public void scrapeJob(Job job) {
-        driver.get(job.url);
-        WebElement jobElement = driver.findElement(By.cssSelector("div.left>div"));
-        String description = jobElement.getAttribute("innerHTML");
+    public boolean scrapeJob(Job job) {
+        try {
+            System.out.println("Scraping " + job.url);
+            resetDriver();
+            driver.get(job.url);
+            Utils.sleepRandom(6);
 
-        job.job_site = "remotive";
-        job.status = "new";
-        job.description = description;
-        job.job_posting_date = LocalDate.now();
+            WebElement jobElement = driver.findElement(By.cssSelector("div.left>div"));
+            String description = jobElement.getAttribute("innerHTML");
 
-        db.storeJob(job);
+            job.job_site = "remotive";
+            job.status = "new";
+            job.description = description;
+            job.job_posting_date = LocalDate.now();
+
+            return db.storeJob(job);
+
+        } catch (Exception e) {
+            System.out.println("Failed to scrape job " + job.url);
+            System.out.println(e.getMessage());
+            return false;
+
+        }
+
+    }
+
+    private void resetDriver() {
+        driver.quit();
+        driver = new ChromeDriver();
+        driver.manage().window().setPosition(new Point(100, 50));
+        driver.manage().window().setSize(new Dimension(1400, 900));
     }
 
 
